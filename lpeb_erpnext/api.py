fd69eeba12b27d807d48eda7f4350349be99db15
Gaurav Naik <gvrvnk@gmail.com>
2017-02-23 15:16:12 +0530

    [Mod] BOM Creation against BOQ

--- lpeb_erpnext/api.py
+++ lpeb_erpnext/api.py
@@ -1,4 +1,5 @@
-import frappe, json
+import frappe
+import json
 from frappe.utils import get_url
 from frappe import _
 
@@ -15,26 +16,74 @@ from frappe import _
 
 #         #frappe.get_all("BOQ Item", fields=["*"],filters=[[parent_item]])
 
+# @frappe.whitelist()
+# def make_bom_for_item(item, boq):
+#     out = ""
+#     children = frappe.get_all("BOQ Item", filters={"parent": boq, "parent_item": item}, fields=["*"], order_by="idx")
+
+#     if len(children) > 0:
+#         b = frappe.new_doc("BOM")
+#         b.item = item
+#         b.qty = frappe.db.get_value("BOQ Item",filters={"parent": boq, "item": item})
+        
+#         print "New BOM for ", item
+#         print "Children: ", "None" if len(children) == 0 else [c.item for c in children]
+
+#         for child in children:
+#             print "Child for ", item, ": Child: ", child
+
+#             # b.append("items", {
+#             #     "item_code": child.item_code,
+#             #     "qty": child.qty
+#             # })
+#             c = make_bom_for_item(child.item, boq)
+#             print c
+
+#         # b.save()
+#         # frappe.db.commit()
+#         out = "BOM created for {0}".format(item)
+#     else:
+#         out = "No BOM for {0}".format(item)
+    
+#     return out
+
 @frappe.whitelist()
-def make_bom_for_item(item, boq):
+def make_boms(boq, project):
+    items = frappe.get_all("BOQ Item", filters={"parent": boq }, fields=["*"], order_by="idx")
+    items = [i["item"] for i in items]
+
+    msgs = []
+
+    for item in items:
+        children = frappe.get_all("BOQ Item", filters={"parent":boq, "parent_item": item}, fields=["*"])
+        if len(children) > 0:
+
+            if len(frappe.get_all("BOM", filters={"item": item, "project": project})) == 0:
+                msg = make_actual_bom(boq, project, item, children)
+                msgs.append(msg)
+            else:
+                msgs.append("BOM already created for item '{0}' under project '{1}'".format(item, project))
+    
+    return "<br>".join(msgs)
+
+def make_actual_bom(boq, project, item, children):
     b = frappe.new_doc("BOM")
+    b.project = project
     b.item = item
     b.qty = frappe.db.get_value("BOQ Item",filters={"parent": boq, "item": item})
 
-    print "New BOM for ", item
-
-    children = frappe.get_all("BOQ Item", filters={"parent": boq, "parent_item": item}, fields=["*"], order_by="idx")
-
-    print "Children: ", "None" if len(children) == 0 else [c.item for c in children]
-
     for child in children:
         b.append("items", {
-            "item_code": child.item_code,
+            "item_code": child.get("item"),
             "qty": child.qty
         })
-        make_bom_for_item(child.item, boq)
 
-    b.save()
-    frappe.db.commit()
+    try:
+        b.save()
+        frappe.db.commit()
+        return "BOM {0} created for item '{1}'".format(b.name, b.item) 
+    except Exception as e:
+        frappe.db.rollback()
+        return ""
 
-    return "Done"
+        
--- lpeb_erpnext/lpeb_erpnext/doctype/boq/boq.js
+++ lpeb_erpnext/lpeb_erpnext/doctype/boq/boq.js
@@ -3,25 +3,29 @@
 
 frappe.ui.form.on('BOQ', {
 	refresh: function(frm) {
-        frm.add_custom_button(__('Create BOM'), function(frm){
-        frappe.call({
-            method: "lpeb_erpnext.api.make_bom_for_item",
-            args: {
-                "item": cur_frm.doc.items[0]["item"],
-                "boq": cur_frm.doc.name
-            },
-            callback: function(r) {
-                // if(!r.exc) {
-                //     frappe.msgprint(r);
-                //     console.log(r);
-                // } else {
-                //     frappe.msgprint(__("not fetched."));
-                // }
-
-            }
+        frm.add_custom_button(__('Create BOMs'), function(frm){
+            frappe.call({
+                method: "lpeb_erpnext.api.make_boms",
+                args: {
+                    "boq": cur_frm.doc.name,
+                    "project": cur_frm.doc.project
+                },
+                callback: function(r) {
+                    console.log(r);
+                    frappe.msgprint(r.message);
+                }
+            });
         });
-    });
-	}
+	},
+    onload: function(frm) {
+        cur_frm.set_query("parent_item", "items", function(doc, cdt, cdn) {
+            return {
+                filters: [
+                    ["item_group", "in", ["Products", "Sub Assemblies"]]
+                ]
+            }
+        }); 
+    }
 });
 
 
