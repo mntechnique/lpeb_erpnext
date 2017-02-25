5c3c37b68eb735ebe2709f2515ddff6200e59a51
saurabh_expo <bhosalesaurabh8@gmail.com>
2017-02-25 16:37:32 +0530

    [WIP] Filters on child table item codes for office_items and shop_floor_items based on the project selected

--- lpeb_erpnext/lpeb_erpnext/doctype/lpeb_dispatch_order/lpeb_dispatch_order.js
+++ lpeb_erpnext/lpeb_erpnext/doctype/lpeb_dispatch_order/lpeb_dispatch_order.js
@@ -3,6 +3,23 @@
 
 frappe.ui.form.on('LPEB Dispatch Order', {
 	refresh: function(frm) {
-
-	}
+        cur_frm.set_query("item_code", "office_items", function() {
+            return {
+            	query: "",
+                filters: [
+                    ["Item", "item_group", "=", "Products"]
+                ]
+            }
+    
+        });
+        cur_frm.set_query("item_code", "shop_floor_items", function() {
+            return {
+            	query: "",
+                filters: [
+                    ["Item", "item_group", "!=", "Products"]
+                ]
+            }
+    
+        });
+    }
 });
