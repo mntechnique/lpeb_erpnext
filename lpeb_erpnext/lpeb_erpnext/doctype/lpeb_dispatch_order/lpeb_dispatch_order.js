82dbf1acbf5c47e1d719dbe9668af0be66ee15ea
Gaurav Naik <gvrvnk@gmail.com>
2017-03-03 12:54:15 +0530

    [Fix] Filters on Office and Shop Floor items

--- lpeb_erpnext/lpeb_erpnext/doctype/lpeb_dispatch_order/lpeb_dispatch_order.js
+++ lpeb_erpnext/lpeb_erpnext/doctype/lpeb_dispatch_order/lpeb_dispatch_order.js
@@ -6,7 +6,7 @@ frappe.ui.form.on('LPEB Dispatch Order', {
         cur_frm.set_query("item_code", "office_items", function() {
             return {
             	query: "lpeb_erpnext.api.bomitems_for_project",
-                filters: {                    
+                filters: {                   
                     "item_group": "Products",
                     "project_name": cur_frm.doc.project
                 }
@@ -17,8 +17,7 @@ frappe.ui.form.on('LPEB Dispatch Order', {
             return {
             	query: "lpeb_erpnext.api.bomitems_for_project",
                 filters: {
-                    // "item_group": "'Raw Materials', 'Sub Assemblies', 'Consumables'"
-                    "item_group": "Raw Material",
+                    "item_group": ["in", ["Raw Material", "Sub Assemblies"]],
                     "project_name": cur_frm.doc.project
                 }
             }
