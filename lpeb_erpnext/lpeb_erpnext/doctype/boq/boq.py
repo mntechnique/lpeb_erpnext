bdad148303d1ad2da6ccf9861a862e42ba0171c8
Gaurav Naik <gvrvnk@gmail.com>
2017-03-10 09:10:19 +0530

    Multiple Changes
    [Fix] BOQ: Code cleanup
    [Fix] LPEB Settings Warehouse Item: Column headings in proper case.

--- lpeb_erpnext/lpeb_erpnext/doctype/boq/boq.py
+++ lpeb_erpnext/lpeb_erpnext/doctype/boq/boq.py
@@ -41,7 +41,6 @@ class BOQ(Document):
 
 	    self.cancel_boms()
 
-	    
 	def submit_boms(self):
 		subassemblies = [i for i in self.items if frappe.db.get_value("Item", i.item, "item_group") == "Sub Assemblies"]
 		
@@ -112,81 +111,3 @@ class BOQ(Document):
 				bom_sa = frappe.get_doc("BOM", bom_sa_name[0].name)
 				bom_sa.cancel()
 				frappe.db.commit()
-
-
-# def build_bom_submit_queue(item, boq, bom_queue):
-#     children = frappe.get_all("BOQ Item", filters={"parent": boq, "parent_item": item}, fields=["item", "parent_item"]) 
-
-#     if len(children) > 0:
-#         bom_queue.append(item)
-
-#         for c in children:
-#             build_bom_submit_queue(c.item, boq, bom_queue)
-
-	
-
-	# b = frappe.new_doc("BOM")
-	# b.item = item_name
-	# b.qty = frappe.db.get_value("BOQ Item",filters={"parent": boq, "item": item})
-
-	# print "New BOM for ", item
-
-	# children = frappe.get_all("BOQ Item", filters={"parent": boq, "parent_item": item}, fields=["*"], order_by="idx")
-
-	# print "Children: ", "None" if len(children) == 0 else [c.item for c in children]
-
-	# for child in children:
-	#     b.append("items", {
-	#         "item_code": child.item_code,
-	#         "qty": child.qty,
-	#         "bom_no": make_bom_for_item(child.item_code, boq)
-	#     })
-		
-	# b.save()
-	# b.submit()
-	# frappe.db.commit()
-
-	# return b.name
-
-	# # on click submit button in boq, by default BOM create based on parent_item of BOQ Item
-	# def on_submit(self):
-	#     print "test test"
-	#     items = frappe.get_all("BOQ Item", filters={"parent": self.name }, fields=["*"], order_by="idx")
-	#     items = [i["item"] for i in items]
-
-	#     msgs = []
-
-	#     for item in items:
-	#         children = frappe.get_all("BOQ Item", filters={"parent":self.name, "parent_item": item}, fields=["*"])
-	#         if len(children) > 0:
-
-	#             if len(frappe.get_all("BOM", filters={"item": item, "project": self.project})) == 0:
-	#                 msg = self.make_actual_bom(item, children)
-	#                 msgs.append(msg)
-	#             else:
-	#                 msgs.append("BOM already created for item '{0}' under project '{1}'".format(item, project))
-
-	#     return "<br>".join(msgs)
-
-	# def make_actual_bom(self, item, children):
-	#     b = frappe.new_doc("BOM")
-	#     b.project = self.project
-	#     b.item = item
-	#     b.qty = frappe.db.get_value("BOQ Item",filters={"parent": self.name, "item": item})
-
-	#     for child in children:
-	#         b.append("items", {
-	#             "item_code": child.get("item"),
-	#             "qty": child.qty
-	#         })
-
-	#     try:
-	#         b.save()
-	#         b.submit()
-	#         frappe.db.commit()
-	#         return "BOM '{0}' created for item '{1}'".format(b.name, b.item)
-
-	#     except Exception as e:
-	#         frappe.db.rollback()
-	#         return ""
-
