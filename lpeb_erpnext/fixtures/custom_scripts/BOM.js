a130cc9fcf5938f7e673cd05c97ea8809adf5ef2
Gaurav Naik <gvrvnk@gmail.com>
2017-04-26 20:01:19 +0530

    [Fix] Dropped Custom Script for BOM

--- lpeb_erpnext/fixtures/custom_scripts/BOM.js
+++
@@ -1,7 +0,0 @@
-frappe.ui.form.on("BOM", {
-	"item_code": function(frm, cdt, cdn) {
-		var row = locals[cdt][cdn];
-		row["bom_no"] == "ABC123";
-		console.log("Tried setting BOMNO", "ABC123");
-	}
-});
